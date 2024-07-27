7d0b51ec8b40c17cd347e675a5855c6285bdc6ef
Lautaro Linquimán <lylinquiman@gmail.com>
2024-07-27 19:38:50 -0300

    Feat: add build dist plugin

---
+++ build_dist.sh
@@ -0,0 +1,10 @@
+npm run build
+
+cp src/*.php dist
+cp -r src/networks dist/
+cp -r src/classes dist/
+cp -r src/templates dist/
+
+cp -r dist social_planner
+zip  social_planner.zip social_planner
+rm -rf social_planner
