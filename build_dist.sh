eec15d6e4b17a33e6e127b83c0f960235cc34ef7
Lautaro Linquimán <lylinquiman@gmail.com>
2024-07-27 20:16:55 -0300

    Add license and readme to dist

--- build_dist.sh
+++ build_dist.sh
@@ -5,6 +5,10 @@ cp -r src/networks dist/
 cp -r src/classes dist/
 cp -r src/templates dist/
 
+
+cp LICENSE dist
+cp readme.txt dist
+
 cp -r dist social_planner
 zip  social_planner.zip social_planner
 rm -rf social_planner
